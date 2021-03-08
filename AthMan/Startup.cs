e2c7e5eb3cf8822f5a669d79d2a5312141743bfb
Nolan Williams <ntwilliams18@gmail.com>
2021-03-08 12:02:24 -0500

    Added navbar to _Layout and removed trailing slashes

--- AthMan/Startup.cs
+++ AthMan/Startup.cs
@@ -18,7 +18,6 @@ namespace AthMan
 
         public IConfiguration Configuration { get; }
 
-        // Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -29,11 +28,10 @@ namespace AthMan
 
             services.AddRouting(options => {
                 options.LowercaseUrls = true;
-                options.AppendTrailingSlash = true;
+                options.AppendTrailingSlash = false;
             });
         }
 
-        // Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -42,15 +40,13 @@ namespace AthMan
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
